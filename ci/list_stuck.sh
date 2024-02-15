436a1e2b281a4dace0721db3cad9e0db0f2f13db
Ilya Kobets <i.kobets@orioledb.com>
2024-02-15 16:51:13 +0100

    added sudo for gdb

--- ci/list_stuck.sh
+++ ci/list_stuck.sh
@@ -2,4 +2,4 @@ pgrep postgres | xargs -r ps
 pgrep memcheck | xargs -r ps
 pgrep python | xargs -r ps
 
-pgrep postgres | xargs -r gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" -p
+pgrep postgres | sudo xargs -r gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" -p
