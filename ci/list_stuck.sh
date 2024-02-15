ef026a83b332c933e93f89f5ee1347267af401e8
Ilya Kobets <i.kobets@orioledb.com>
2024-02-15 13:53:47 +0100

    Added additional output

--- ci/list_stuck.sh
+++ ci/list_stuck.sh
@@ -1,2 +1,3 @@
 pgrep postgres | xargs ps
 pgrep memcheck | xargs ps
+pgrep python | xargs ps
--- t/base_test.py
+++ t/base_test.py
@@ -71,7 +71,7 @@ class BaseTest(unittest.TestCase):
 		self.node.append_conf('postgresql.conf',
 		                      "shared_preload_libraries = orioledb\n")
 
-		print(f"TEST: {self.__class__}, DIR: {self.node.base_dir}")
+		print(f"TEST: {self.__class__}, DIR: {self.node.base_dir}, PID: {os.getpid()}")
 
 	def list2reason(self, exc_list):
 		if exc_list and exc_list[-1][0] is self:
