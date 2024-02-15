58ac3d351305f674eb392c778e0179c21b136bb4
Ilya Kobets <i.kobets@orioledb.com>
2024-02-15 18:02:59 +0100

    changed bash to postgres in pgrep

--- ci/list_stuck.sh
+++ ci/list_stuck.sh
@@ -2,4 +2,4 @@ pgrep postgres | xargs -r ps
 pgrep memcheck | xargs -r ps
 pgrep python | xargs -r ps
 
-pgrep bash | sudo xargs -r -I{} bash -c 'ps {}; gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" -p {}'
+pgrep postgres | sudo xargs -r -I{} bash -c 'ps {}; gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" -p {}'
