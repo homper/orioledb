5d4f6ac80236ad3687bdf3c6945213c78ee6c8f9
Ilya Kobets <i.kobets@orioledb.com>
2025-03-11 00:08:50 +0100

    Try this

--- ci/gcov.sh
+++ ci/gcov.sh
@@ -6,12 +6,10 @@ set -x
 
 cd orioledb
 if [ $COMPILER = "clang" ]; then
-	llvm-cov-$LLVM_VER gcov src/*.c src/*/*.c include/*.h include/*/*.h -r -p -l
-	# find . -type f -name '*.gcno' -exec llvm-cov-$LLVM_VER gcov -r -l -pb {} +
+	# llvm-cov-$LLVM_VER gcov src/*.c src/*/*.c include/*.h include/*/*.h -r -p -l
+	bash -c 'find . -type f -name '\''*.gcno'\'' -exec llvm-cov-$LLVM_VER gcov -r -p -l -b {} +'
 else
-	find . -type f -name "*.gcno"
-	find . -type f -name "*.gcda"
-	gcov src/*.c src/*/*.c -r -p -l
-	# bash -c 'find . -type f -name '\''*.gcno'\''  -not -path '\''./bower_components/**'\'' -not -path '\''./node_modules/**'\'' -not -path '\''./vendor/**'\'' -exec gcov -pb  {} +'
+	# gcov src/*.c src/*/*.c -r -p -l
+	bash -c 'find . -type f -name '\''*.gcno'\'' -exec gcov -r -p -l -b {} +'
 fi
 cd ..
