405a53c679b63f4991ba54853175a61cde8dd788
Ilya Kobets <i.kobets@orioledb.com>
2025-03-10 23:38:09 +0100

    relative gcov

--- ci/gcov.sh
+++ ci/gcov.sh
@@ -6,10 +6,12 @@ set -x
 
 cd orioledb
 if [ $COMPILER = "clang" ]; then
-	# llvm-cov-$LLVM_VER gcov src/*.c src/*/*.c include/*.h include/*/*.h -r -p -l
-	find . -type f -name '*.gcno' -exec llvm-cov-$LLVM_VER gcov -r -l -pb {} +
+	llvm-cov-$LLVM_VER gcov src/*.c src/*/*.c include/*.h include/*/*.h -r -p -l
+	# find . -type f -name '*.gcno' -exec llvm-cov-$LLVM_VER gcov -r -l -pb {} +
 else
-	# gcov src/*.c src/*/*.c -r -p -l
-	bash -c 'find . -type f -name '\''*.gcno'\''  -not -path '\''./bower_components/**'\'' -not -path '\''./node_modules/**'\'' -not -path '\''./vendor/**'\'' -exec gcov -pb  {} +'
+	find . -type f -name "*.gcno"
+	find . -type f -name "*.gcda"
+	gcov src/*.c src/*/*.c -r -p -l
+	# bash -c 'find . -type f -name '\''*.gcno'\''  -not -path '\''./bower_components/**'\'' -not -path '\''./node_modules/**'\'' -not -path '\''./vendor/**'\'' -exec gcov -pb  {} +'
 fi
 cd ..
