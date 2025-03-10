53a773b60b3adcd7be384bf3d3ca9434635709ae
Ilya Kobets <i.kobets@orioledb.com>
2025-03-11 00:39:26 +0100

    remove other gcov files

--- ci/gcov.sh
+++ ci/gcov.sh
@@ -7,9 +7,10 @@ set -x
 cd orioledb
 if [ $COMPILER = "clang" ]; then
 	# llvm-cov-$LLVM_VER gcov src/*.c src/*/*.c include/*.h include/*/*.h -r -p -l
-	bash -c 'find . -type f -name '\''*.gcno'\'' -exec llvm-cov-$LLVM_VER gcov -r -p -l -b {} +'
+	bash -c 'find . -type f -name '\''*.gcno'\'' -exec llvm-cov-$LLVM_VER gcov -p -l -b {} +'
 else
 	# gcov src/*.c src/*/*.c -r -p -l
-	bash -c 'find . -type f -name '\''*.gcno'\'' -exec gcov -r -p -l -b {} +'
+	bash -c 'find . -type f -name '\''*.gcno'\'' -exec gcov -p -l -b {} +'
 fi
+bash -c 'find . \( -type f -name '\''*.gov'\'' \) -and -not \( -name '\''src#*'\'' -o -name '\''include#*'\'' \) -exec llvm-cov-$LLVM_VER gcov -r -p -l -b {} +'
 cd ..
